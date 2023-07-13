23cc017721f565b9d0d9dc3a7d8b66c6b21457ac
Anouarziane <zianeanoire@gmail.com>
2023-07-13 15:07:23 -0700

    aaah

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main(void) - describe
+*
+* this is a success
+*
+* main - describe
+*
+* this is a function
+*
+* return a 0
+*
+* this is a sucsses
+*
+*/
+int main(void)
+{
+	/* this is a descr */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); /* this is a description */
+	/* this is a description */
+}
