b2400daf7add397cfda2727f8261ea6fb0a8d033
Anouarziane <zianeanoire@gmail.com>
2023-07-18 13:05:31 -0700

    go

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet _ Prints the alphabet in lowercase
+  *
+  * Return: always 0 Success
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
