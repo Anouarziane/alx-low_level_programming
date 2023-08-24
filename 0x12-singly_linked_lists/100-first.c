6eea4704b49c8de494bf72e44eac7c70e1e80500
Anouarziane <zianeanoire@gmail.com>
2023-08-24 15:03:19 -0700

    aaaa

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,9 +10,3 @@ void before_main(void)
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
 }
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
