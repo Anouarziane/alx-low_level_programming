f2c9e8f90c137fb9259b459d41a6a76bf5046146
Anouarziane <zianeanoire@gmail.com>
2023-08-31 14:37:19 -0700

    aasas

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index To 1
- * @n: pointer to the number to change now
- * @index: index of The bit to set to 1 now
+ * set_bit - sets a bit aT a the giVen inDex to 1
+ * @n: pointer to  numBer to ChaNge in index
+ * @index: inDex of the bit to Set tO 1
  *
- * Return: 1 for sUcCess, -1 for failure
+ * Return: 1 for suCcess and -1 for faiLure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
+	if (index > 63)
+		return (-1);
 
-    unsigned long int mask = 1UL << index;
-    *n |= mask;
-
-    return (1);
+	*n = (*n | (1UL << index));
+	return (1);
 }
